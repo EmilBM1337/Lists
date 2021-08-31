4c499f70b1d409ab242c90f94a88e769300b7622
emil41m5 <emil41m5@CV-BB-5340>
2021-08-31 11:40:17 +0200

    .

--- Opgave 2/Program.cs
+++ Opgave 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Opgave_2
 {
@@ -6,7 +7,28 @@ namespace Opgave_2
     {
         static void Main(string[] args)
         {
-            
+            List<string> names = new List<string>() { "Hans", "Gerda", "Knud", "Karsten", "Irene"};
+            names.Insert(3, "Anders");
+            names.Insert(4, "Johanne");
+
+            List<int> ages = new List<int>() { 13, 14, 13, 15, 13, 14, 14, 15 };
+            ages.Remove(13);
+            ages.Remove(15);
+            ages.RemoveAt(3);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("");
+
+            foreach (int age in ages)
+            {
+                Console.WriteLine(age);
+            }
+
+            Console.ReadKey();
         }
     }
 }
